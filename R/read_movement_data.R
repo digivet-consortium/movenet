ffdc9582b6a407bac359d3f23ccc31f23d7f4232
CarlijnB <cbogaardt@gmail.com>
2022-04-26 11:54:20 +0100

    changed import mechanism

--- R/read_movement_data.R
+++ R/read_movement_data.R
@@ -2,6 +2,7 @@
 #'
 #' @param move_data_file a delimited file with movement data
 #'
+#' @importFrom data.table fread
 #' @return reformatted movement data (selected & renamed columns)
 #' @export
 #'
@@ -9,7 +10,7 @@
 reformat_move_data <- function(move_data_file){
   source("config.R") #better as argument to function, or turned into .yml file?
   varlist <- c(move_ID,origin_ID,dest_ID,dep_date,arr_date,nr_pigs)
-  data.table::fread(move_data_file, select=varlist,
+  fread(move_data_file, select=varlist,
         col.names=c("move_ID","origin_ID","dest_ID","dep_date","arr_date","nr_pigs")
   )
   }
