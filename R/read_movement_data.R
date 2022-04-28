e1669e9cda0aeb6b87a5c67e8699503786fc4fdf
CarlijnB <cbogaardt@gmail.com>
2022-04-28 19:51:30 +0100

    Updated read_movement_data Switched to using readr::read_delim, with optional user-defined delimiter and date formats. Also set column types: various IDs as character, dates as datetimes, nr of pigs as integer

--- R/read_movement_data.R
+++ R/read_movement_data.R
@@ -2,17 +2,24 @@
 #'
 #' @param move_data_file a delimited file with movement data
 #'
-#' @importFrom data.table fread
+#' @importFrom readr read_delim
+#'
 #' @return reformatted movement data (selected & renamed columns)
 #' @export
 #'
 #' @examples
-reformat_move_data <- function(move_data_file){
-  source("config.R") #better as argument to function, or turned into .yml file?
-  varlist <- c(move_ID,origin_ID,dest_ID,dep_date,arr_date,nr_pigs)
-  fread(move_data_file, select=varlist,
-        col.names=c("move_ID","origin_ID","dest_ID","dep_date","arr_date","nr_pigs")
-  )
-  }
+reformat_move_data <- function(move_data_file, delim = NULL, datetime_format = ""){
 
-#Dates: Make date format consistent: YYYY-MM-DD , YYYYMMDD , separate columns?
+  source("R/config.R") #better as argument to function, or turned into .yml file?
+  varlist <- c(move_ID,origin_ID,dest_ID,dep_date,arr_date,nr_pigs)
+
+  read_delim(move_data_file, delim = delim,
+             col_select = eval(varlist),
+             col_types = cols(.default = col_character())
+             ) %>%
+    `colnames<-`(c("move_ID","origin_ID","dest_ID","dep_date","arr_date","nr_pigs")) %>%
+    mutate(dep_date = parse_datetime(dep_date, format = datetime_format),
+           arr_date = parse_datetime(arr_date, format = datetime_format),
+           nr_pigs = as.integer(nr_pigs))
+
+  }
