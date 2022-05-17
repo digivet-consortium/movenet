57d97ef7ee72e61908621daa0c7eeb186aa5d909
CarlijnB <cbogaardt@gmail.com>
2022-05-17 18:35:08 +0100

    Restructured validate_config.R
    This is now an outward-facing function, calling an internal function, in turn calling multiple separate testing functions.
    The testing functions each raise "messages" for invalid conditions. These are (invisibly) collected by the internal function. If the config file is invalid, the external function raises an error and additionally prints the collected messages.

--- R/validate_config.R
+++ R/validate_config.R
@@ -11,76 +11,86 @@
 #'
 #' @export
 validate_config <- function(file){
-  if (internal_validate_config(file) == FALSE){
-    stop(paste(file,"is not a valid movenet config file"), call. = FALSE)
-  }
-  else{
+  failed_validation_messages = suppressMessages(internal_validate_config(file))
+  if (is.null(failed_validation_messages)){
     invisible(TRUE)
   }
-}
+  else{
+    stop(
+      sprintf(paste(file,"is not a valid movenet config file\n%s"), paste0(failed_validation_messages, collapse="")),
+      call. = FALSE
+    )}
+  }
+
 
 internal_validate_config <- function(file){
-  if (is_yaml_file(file)==FALSE){
-    return(FALSE)
+  msg <- NULL
+  suppressMessages(
+    withCallingHandlers({
+      if (validate_yaml(file) == TRUE){
+        yamlfile <- yaml.load_file(file)
+        validate_config_root(yamlfile)
+        validate_config_move(yamlfile)
+        #validate_config_holding(yamlfile)
+      }},
+      message = function(m) {
+        msg <<- append(msg, conditionMessage(m))
+        }
+  ))
+  invisible(msg)
+}
+
+#adapted from https://rdrr.io/cran/validate/src/R/yaml.R
+validate_yaml <- function(file){
+  out <- tryCatch(yaml.load_file(file),error = function(e) FALSE)
+  test<-!identical(out,FALSE)
+  if(test==FALSE){
+    message(paste(file,"is not valid yaml format"))
   }
-  else{
-    test_config_structure(file)
+  invisible(test)
+}
+
+validate_config_root <- function(yamlfile){
+  root_keys_obs <- names(yamlfile)
+  root_keys_exp <- c("movement_data","holding_data")
+  root_valid <-  length(root_keys_obs) > 1 && all(root_keys_exp %in% root_keys_obs)
+  if (!root_valid){
+    message(
+      sprintf("Missing mandatory top-level keys: %s", paste0(root_keys_exp[!root_keys_exp %in% root_keys_obs],collapse=", "))
+    )
   }
 }
 
-test_config_structure <- function(file){
-  yamlfile <- yaml.load_file(file) #requires yaml package
-  root_keys_obs <- names(yamlfile)
-  root_keys_exp <- c("movement_data","holding_data")
-  root_valid <-  length(root_keys_obs) > 1 && all(root_keys_exp %in% root_keys_obs) #tests move & holding data in yaml headings, file may have more
-
+validate_config_move <- function(yamlfile){
   move_keys_obs <- names(yamlfile[["movement_data"]])
   move_keys_exp <- c("move_ID", "origin_ID", "dest_ID", "move_date", "nr_pigs")
   move_notmissing <- length(move_keys_obs) > 4 && all(move_keys_exp %in% move_keys_obs) #tests that required move keys are present; but file may have more keys
   move_allchar <- all(sapply(yamlfile[["movement_data"]][move_keys_obs %in% move_keys_exp],is.character)) #tests that required & non-missing move values are all characters
   move_valid <- all(move_notmissing && move_allchar)
-
-  #node_keys_obs <- names(yamlfile[["holding_data"]])
-  #add tests here once clear what we want with holding data
-
-  if (!root_valid){
-    warning(
-      sprintf("Missing top-level keys: %s\n", paste0(root_keys_exp[!root_keys_exp %in% root_keys_obs],collapse=", ")),
-      call. = FALSE
-    )
-  }
   if (!move_notmissing){
-    warning(
-      sprintf("Missing movement_data keys: %s\n", paste0(move_keys_exp[!move_keys_exp %in% move_keys_obs],collapse=", ")),
-      call. = FALSE
+    message(
+      sprintf("Missing mandatory movement_data keys: %s", paste0(move_keys_exp[!move_keys_exp %in% move_keys_obs],collapse=", "))
     )
   }
-  #if (!node_notmissing){
-  #  warning(
-  #    sprintf("Missing holding_data keys: %s\n", paste0(node_keys_exp[!node_keys_exp %in% node_keys_obs],collapse=", ")),
-  #    call. = FALSE
+  if (!move_allchar){ #Needs adapting when holding data is incorporated
+    message(
+      sprintf("Data fields not in expected character format: %s", paste0(names(which(!sapply(yamlfile[["movement_data"]][move_keys_obs %in% move_keys_exp],is.character))),collapse=", "))
+    )
+  }
+}
+
+#validate_config_holding <- function(yamlfile){
+  #holding_keys_obs <- names(yamlfile[["holding_data"]])
+  #holding_keys_exp <-  # ADD MANDATORY KEYS!
+  #holding_notmissing <- length(move_keys_obs) > x && all(holding_keys_exp %in% holding_keys_obs) # SUBSTITUTE X!  tests that required holding keys are present; but file may have more keys
+
+  #if (!holding_notmissing){
+  #  message(
+  #    sprintf("Missing mandatory holding_data keys: %s", paste0(holding_keys_exp[!holding_keys_exp %in% holding_keys_obs],collapse=", "))
   #  )
   #}
-  if (!move_allchar){ #Needs adapting when holding data is incorporated
-    warning(
-      sprintf("Data fields not in expected character format: %s\n", paste0(names(which(!sapply(yamlfile[["movement_data"]][move_keys_obs %in% move_keys_exp],is.character))),collapse=", ")),
-      call. = FALSE
-    )
-  }
+#}
 
-  valid <- all(root_valid && move_valid) #add test for holding data here too
 
-  return(valid)
-}
 
-#adapted from https://rdrr.io/cran/validate/src/R/yaml.R
-is_yaml_file <- function(file){
-  out <- tryCatch(yaml.load_file(file),error = function(e) FALSE)
-  test<-!identical(out,FALSE)
-  if(test==FALSE){
-    warning("Input file not valid yaml format", call. = FALSE)
-  }
-  return(test)
-}
-#Accepts lots of formats that can be read as yaml (e.g. .md or .R files)
-#When faced with e.g. pdf, it prints warnings that aren't helpful. How do I silence these?
+
