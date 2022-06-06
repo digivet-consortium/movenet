02fd6e288e7c824edcea6aafaa2b1fe489e1f664
CarlijnB <cbogaardt@gmail.com>
2022-06-06 16:23:35 +0100

    Changed structure of test_run_validate_config.R
    Brought the test inside of the loop, to allow continuation of loop after failure of a single test

--- tests/testthat/test_run_validate_config.R
+++ tests/testthat/test_run_validate_config.R
@@ -1,5 +1,6 @@
-test_that("all config files in configurations folder are valid",{
-  for (config in Sys.glob(paste0(system.file("configurations",package="movenet"),"/*.yml"))){
+for (config in Sys.glob(paste0(system.file("configurations",package="movenet"),"/*.yml"))){
+  test_that(paste(config, "is a valid config file"),{
     expect_true(validate_config(!!config))
-   }
- })
+  }
+  )
+}
