464f60d3fd3602db488ae91deb5f15054733d03d
CarlijnB <cbogaardt@gmail.com>
2022-06-28 23:05:58 +0100

    reformat_move_data: col selection as separate step
    First read in all data columns, then use separate function to check for presence of & select columns of interest. If a column of interest is missing, either stop w informative error message (if minvar/ mandatory col) or raise warning and proceed w/o missing column (if extra/optional col)

--- R/read_movement_data.R
+++ R/read_movement_data.R
@@ -24,38 +24,53 @@ reformat_move_data <- function(move_data_file, delim = NULL, datetime_format = "
   if (!all(min_move_keys %in% names(movenetenv$options$movement_data))){
     stop(sprintf("Unexpected config structure. Missing mandatory movement_data keys: %s", paste0(min_move_keys[!min_move_keys %in% names(movenetenv$options$movement_data)],collapse=", ")))
   }
-  minvars <- movenetenv$options$movement_data[min_move_keys]
 
-  extra <- movenetenv$options$movement_data[is.na(match(names(movenetenv$options$movement_data),c("movenet.origin_ID","movenet.dest_ID","movenet.move_date","movenet.nr_pigs")))]
+  #identify columns of interest
+  minvars <- movenetenv$options$movement_data[min_move_keys] #mandatory
+  extra <- movenetenv$options$movement_data[is.na(match(names(movenetenv$options$movement_data),min_move_keys))] #optional
 
+  #read in datafile (all columns), with col type for mandatory columns (minvars) initially set as character
+  minvar_coltypes <- list("c", "c", "c", "c") #this creates named list with col type for minvars
+  names(minvar_coltypes) <- unlist(unname(minvars))
+  all_data <- read_delim(move_data_file, delim = delim,
+                         col_types = minvar_coltypes #this guesses col type when not specified, i.e. for extra variables
+                         )
 
-  #create list with column types for minimal variables - first keep all minvars as character, then change in separate step below
-  minvar_coltypes <- list("c", "c", "c", "c")
-  names(minvar_coltypes) <- lapply(unname(minvars),FUN=as.name)
+  #select columns of interest
+  selected_data <- select_cols(data = all_data, minvars = minvars, extra = extra)
 
-  #read in selected columns, with min cols as character type
-  selected_data <- read_delim(move_data_file, delim = delim,
-                              col_select = unname(unlist(c(minvars,extra))),
-                              col_types = minvar_coltypes #this guesses column type when not specified, i.e. for extra variables
-                              )
-  #Here need to trycatch and (i) if minvar missing, error; (ii) if extra var missing, warning + take this out of the col_select list and run again
-  #Or: first read in ALL columns, then select columns afterwards
-
-  #check and change col types for nr_pigs and move_date; or raise informative errors
+  #check data & change col types for nr_pigs and move_date; or raise informative errors
   selected_data[minvars$movenet.nr_pigs] <- reformat_nrpigs(selected_data[minvars$movenet.nr_pigs])
   selected_data[minvars$movenet.move_date] <- reformat_date(selected_data[minvars$movenet.move_date], datetime_format = datetime_format)
 
   return(selected_data)
 }
 
+select_cols <- function(data, minvars, extra){
+  if (!(all(unlist(minvars) %in% colnames(data)))){
+    missing_minvars <- unname(unlist(minvars))[which(!(unlist(minvars) %in% colnames(data)))]
+    stop(sprintf("Can't find the following mandatory columns in the datafile: %s.", paste0(missing_minvars, collapse=", ")), call. = FALSE)
+  }
+  if (!(all(unlist(extra) %in% colnames(data)))){
+    missing_extra <- unname(unlist(extra))[which(!(unlist(extra) %in% colnames(data)))]
+    warning(sprintf("Can't find the following requested optional columns in the datafile: %s.\n Proceeding without missing optional columns.",
+                    paste0(missing_extra, collapse=", ")),
+            call. = FALSE)
+    to_extract <- unname(unlist(c(minvars,extra)))[-which(unname(unlist(c(minvars,extra))) %in% missing_extra)]
+  }else{
+    to_extract <- unname(unlist(c(minvars,extra)))
+  }
+  data[to_extract]
+}
+
 reformat_nrpigs <- function(nr_pigs_col){
   tryCatch(
     error = function(cnd) {
       cnd$message <- paste0("Column `",colnames(nr_pigs_col),"` must be an integer: it can't contain a decimal or grouping mark.")
+      cnd$call <- NULL
       stop(cnd)
     },
-    withr::with_options(list(warn=2),parse_integer(nr_pigs_col[[colnames(nr_pigs_col)]]))
-  )
+    withr::with_options(list(warn=2),parse_integer(nr_pigs_col[[colnames(nr_pigs_col)]])))
 }
 
 reformat_date <- function(date_col, datetime_format){
@@ -82,11 +97,12 @@ reformat_date <- function(date_col, datetime_format){
                                     "` contains one or more invalid dates.\nSee `?parse_datetime` for guidance on readr date(time) format specifications.\nOriginal readr warning message:\n",old_message))
       }
       cnd$message <- paste0(msg, paste0(msglist,collapse="\nIn addition:\n"))
+      cnd$call <- NULL
       stop(cnd)
     },
     withr::with_options(list(warn=2),
                         parse_datetime(date_col[[colnames(date_col)]], format = datetime_format))
-  )
+    )
 }
 
 
