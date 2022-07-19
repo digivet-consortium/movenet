7adb34887946aecabce3dfadcd3546a6b47e8a5a
CarlijnB <cbogaardt@gmail.com>
2022-07-19 13:46:28 +0100

    Read_movement_data.R: use fileopts from config + read dates
    - use separator, encoding, decimal mark and date format as specified in config rather than passed in as arguments
    
    - read dates instead of datetimes

--- R/read_movement_data.R
+++ R/read_movement_data.R
@@ -1,8 +1,6 @@
 #' Reformats movement data from a delimited file into a common intermediate format (extracts and renames selected columns)
 #'
 #' @param move_data_file Path to a delimited file with movement data (alternatively: literal data or a connection).
-#' @param delim Character used as delimiter between fields (optional). The default value (NULL) results in an automatic guess based on the first 1000 lines of data.
-#' @param datetime_format Datetime format specification, as described for readr::parse_datetime. The default value ("") results in datetimes being parsed as ISO8601. The format specification must match the complete string.
 #'
 #' @importFrom dplyr mutate
 #' @importFrom magrittr %>%
@@ -14,25 +12,31 @@
 #'
 #' @examples
 #'
-reformat_move_data <- function(move_data_file, delim = NULL, datetime_format = ""){
+reformat_move_data <- function(move_data_file){
 
   if (!file.exists(move_data_file)){
     stop(paste0(move_data_file, ": no such file exists"))
   }
 
-  min_move_keys <- c("movenet.origin_ID", "movenet.dest_ID", "movenet.move_date", "movenet.nr_pigs")
-  if (!all(min_move_keys %in% names(movenetenv$options$movement_data))){
-    stop(sprintf("Unexpected config structure. Missing mandatory movement_data keys: %s", paste0(min_move_keys[!min_move_keys %in% names(movenetenv$options$movement_data)],collapse=", ")))
+  min_move_keys <- c("origin_ID", "dest_ID", "move_date", "nr_pigs")
+  if (!all(min_move_keys %in% names(movenetenv$options$movedata_cols))){
+    stop(sprintf("Unexpected config structure. Missing mandatory movedata_cols keys: %s", paste0(min_move_keys[!min_move_keys %in% names(movenetenv$options$movedata_cols)],collapse=", ")))
   }
 
   #read in datafile (all columns), with col type initially character for all columns
-  all_data <- read_delim(move_data_file, delim = delim, col_types = cols(.default = col_character()), lazy = TRUE)
+  all_data <- read_delim(move_data_file,
+                         delim = movenetenv$options$movedata_fileopts$separator,
+                         locale = locale(date_format = movenetenv$options$movedata_fileopts$date_format,
+                                         decimal_mark = movenetenv$options$movedata_fileopts$decimal,
+                                         encoding = movenetenv$options$movedata_fileopts$encoding),
+                         col_types = cols(.default = col_character()),
+                         lazy = TRUE)
 
   #select columns of interest
-  minvars <- movenetenv$options$movement_data[min_move_keys] #mandatory
-  extra <- movenetenv$options$movement_data[is.na(match(names(movenetenv$options$movement_data),min_move_keys))] #optional
+  minvars <- movenetenv$options$movedata_cols[min_move_keys] #mandatory
+  extra <- movenetenv$options$movedata_cols[is.na(match(names(movenetenv$options$movedata_cols),min_move_keys))] #optional
   #convert options with integer values (column indices) to column names
-  if(any(sapply(movenetenv$options$movement_data,is.integer))){
+  if(any(sapply(movenetenv$options$movedata_cols,is.integer))){
     opt_w_names <- colindex2name(data = all_data, minvars = minvars, extra = extra)
     minvars <- opt_w_names[[1]]
     extra <- opt_w_names[[2]]
@@ -40,12 +44,14 @@ reformat_move_data <- function(move_data_file, delim = NULL, datetime_format = "
   selected_data <- select_cols(data = all_data, minvars = minvars, extra = extra)
 
   #check data & change col types; or raise informative errors
-  selected_data[minvars$movenet.nr_pigs] <- reformat_nrpigs(selected_data[minvars$movenet.nr_pigs])
-  selected_data[minvars$movenet.move_date] <- reformat_date(selected_data[minvars$movenet.move_date], datetime_format = datetime_format)
+  selected_data[minvars$nr_pigs] <- reformat_nrpigs(selected_data[minvars$nr_pigs])
+  selected_data[minvars$move_date] <- reformat_date(selected_data[minvars$move_date])
   if (length(selected_data) > 4){
-    selected_data[unlist(extra[extra %in% names(selected_data)])] <- suppressMessages(type_convert(selected_data[unlist(extra[extra %in% names(selected_data)])])) #guess coltype of extra columns
+    selected_data[unlist(extra[extra %in% names(selected_data)])] <-
+      suppressMessages(type_convert(selected_data[unlist(extra[extra %in% names(selected_data)])], #guess coltype of extra columns
+                                    locale = locale(date_format = movenetenv$options$movedata_fileopts$date_format,
+                                                    decimal_mark = movenetenv$options$movedata_fileopts$decimal)))
   }
-
   return(selected_data)
 }
 
@@ -98,35 +104,32 @@ reformat_nrpigs <- function(nr_pigs_col){
     withr::with_options(list(warn=2),parse_integer(nr_pigs_col[[colnames(nr_pigs_col)]])))
 }
 
-reformat_date <- function(date_col, datetime_format){
+reformat_date <- function(date_col){
+  date_format <- movenetenv$options$movedata_fileopts$date_format
   tryCatch(
     error = function(cnd) {
       old_message <- cnd$message
       msg <- paste0("Can't parse column `",colnames(date_col),"` as date.\n")
       msglist <- c()
-      #if datetime format string is invalid (not a recognisable datetime format string)
-      if(!grepl("%(Y|y|AD|D|F|x|s)|^$",datetime_format)){
-         msglist <- c(msglist,paste0("`datetime_format` must match readr date(time) format specifications; its specified value `",datetime_format,"` doesn't.\nSee `?parse_datetime` for guidance."))
-      }
       #if a date column contains strings with no numbers (e.g. "foo")
       if(!any(grepl("[0-9]",date_col[[colnames(date_col)]]))){
-         msglist <- c(msglist,paste0("Column `",colnames(date_col),"` does not contain any numbers.\nHave you identified the correct column name under the option `movenet.move_date`?"))
+         msglist <- c(msglist,paste0("Column `",colnames(date_col),"` does not contain any numbers.\nHave you identified the correct column name under the option `move_date`?"))
       }
       #Otherwise (difficult to assess what specific situation applies)
       #- other forms of "date column can simply not be a date" - e.g. 34/345/12
-      #- if datetime format string is invalid (format does not match data) - e.g. %m%d%Y when it is %d%m%Y
-      #- if datetime format string is missing (format is incorrectly interpreted as iso)
+      #- if date format string is invalid (format does not match data) - e.g. %m%d%Y when it is %d%m%Y
+      #- if date format string is missing (format is incorrectly interpreted as iso)
       #- if a date column contains some invalid dates - e.g. 30 Feb
       if(length(msglist)==0){
-        msglist <- c(msglist,paste0("The specified `datetime_format` (value `",datetime_format,"`) and the actual format of column `",colnames(date_col),"` don't appear to match.\nAlternatively, column `",colnames(date_col),
-                                    "` contains one or more invalid dates.\nSee `?parse_datetime` for guidance on readr date(time) format specifications.\nOriginal readr warning message:\n",old_message))
+        msglist <- c(msglist,paste0("The date format specification given through the option `date_format` (value `",date_format,"`) and the actual format of column `",colnames(date_col),"` don't appear to match.\nAlternatively, column `",colnames(date_col),
+                                    "` contains one or more invalid dates.\nSee `readr::?parse_date` for guidance on readr date format specifications.\nOriginal readr warning message:\n",old_message))
       }
       cnd$message <- paste0(msg, paste0(msglist,collapse="\nIn addition:\n"))
       cnd$call <- NULL
       stop(cnd)
     },
     withr::with_options(list(warn=2),
-                        parse_datetime(date_col[[colnames(date_col)]], format = datetime_format))
+                        parse_date(date_col[[colnames(date_col)]], format = date_format))
     )
 }
 
