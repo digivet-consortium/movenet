125a9c266c3f4dab05f787475663eee878f9b72d
CarlijnB <cbogaardt@gmail.com>
2022-07-29 16:05:00 +0100

    Added non-geogr coarsening & anonymisation functions
    These still require cleaning up, checks, testing

--- R/coarsening_functions.R
+++ R/coarsening_functions.R
@@ -1,11 +1,19 @@
-coarsen_date <- function(data, level){
+#' Title
+#'
+#' @param data
+#' @param level
+#' @param aggregate_data
+#'
+#' @return
+#' @export
+#'
+#' @importFrom dplyr group_by mutate summarise ungroup
+#' @importFrom lubridate floor_date
+#' @importFrom magrittr %>%
+#'
+#' @examples
+coarsen_date <- function(data, level, aggregate_data = TRUE, ...){
   # accesses each date on an edge in network, changes to level aggregation
-  # just coarsen the date or aggregate by date?
-
-  # At what stage to do this? data and/or graph?
-
-  ##Coarsening
-
   coarsened_data <- data %>% mutate("{movenetenv$options$movedata_cols$move_date}" := floor_date(.data[[movenetenv$options$movedata_cols$move_date]], level)) # result is date object, rounded down to start of unit (e.g. month)
 
   #Alternatives:
@@ -13,47 +21,60 @@ coarsen_date <- function(data, level){
   # format(date_object, format_string) from base R -> character vector matching format_string (e.g %Y-%m)
   # zoo::as.yearmon(date_object) -> yearmon object
 
-  ##Aggregating
-
-  # Aggregate over consistent periods of x number of days (as Lentz has done), or extract year-mon, year-quarter, etc?
+  # Allow for aggregation over consistent periods of x number of days (as Lentz has done)?
   # Lentz uses 1d, 7d (weekly), 14d (fortnightly), 28d (monthly), 84d (quarterly) ?
 
-  # can aggregate nr_pigs by summing, but what for other, non-standardised columns?
-  # have user provide named list with functions? if so, also include nr_pigs, or assume this is always summed up?
-  aggregated_data <-
-    coarsened_data %>%
+  if (aggregate_data == FALSE){
+    if(...length() != 0) warning(paste("As aggregate_data == FALSE, requested summarising functions for the following data columns were ignored:", paste(...names(),collapse=", ")), call. = FALSE)
+    return(coarsened_data)
+  } else {
+    aggregated_data <-
+      coarsened_data %>%
       group_by(.data[[movenetenv$options$movedata_cols$origin_ID]], .data[[movenetenv$options$movedata_cols$dest_ID]], .data[[movenetenv$options$movedata_cols$move_date]]) %>%
-      summarise("{movenetenv$options$movedata_cols$nr_pigs}" := sum(.data[[movenetenv$options$movedata_cols$nr_pigs]])) %>%
+      summarise("{movenetenv$options$movedata_cols$nr_pigs}" := sum(.data[[movenetenv$options$movedata_cols$nr_pigs]]), ...) %>%
       ungroup
-  #Alternative:
-  # Within graph analysis, can use igraph's simplify() with custom functions
+    return(aggregated_data)
+  }
+#Mind this only SUMS pigs, ... is for additional columns only (can't pass an alternative summary function for pigs)
+#Mind this drops columns for which a summary function is not given
 }
 
-
-
-coarsen_weight <- function(data, round, jitter){
+#' @param data
+#'
+#' @param column
+#' @param jitter
+#' @param round
+#'
+#' @export
+coarsen_weight <- function(data, column = movenetenv$options$movedata_cols$nr_pigs, jitter, round){
   # for each weight on an edge in network, apply ± jitter in range jitter, then round to nearest round
   # might want user to be able to just jitter, just round, or have both
-  # round then jitter, or jitter then round (tbh I don't see the point of the jitter in the latter)?
   # sensible default values?
 
-  # The below requires data to be a particular column, e.g. nr_pigs
+  # The below requires column to be a single column.
+  # NB jitter may result in negative numbers - what then?
+  # NB jitter adds decimal numbers
+  # to not need factor, need amount to be >0
+  # Add argument checks with checkmate
 
-  ## jitter
-  # suggest: FALSE for no jitter, or...
-  # use base R jitter defaults? or alternative jitter function?
-  if (jitter != FALSE){
-    data <- jitter(data, factor, amount = jitter) # what about factor?
+  if (!identical(jitter, FALSE)){
+    data[column] <- jitter(data[[column]], amount = jitter) # what about factor?
   }
-
-  ## round
-  # suggest: FALSE for no rounding, or power-of-ten to round to
-  if (round != FALSE){
-    data <- round(data, digits = -(round))
+  if (!identical(round, FALSE)){
+    data[column] <- round(data[[column]], digits = -(round))
   }
-  # (note rounding rule implementation is dependent on OS services and on representation error)
+  return(data)
+}
 
-  #coarsened_weight <-
-  #  data %>%
-  #  mutate("{movenetenv$options$movedata_cols$nr_pigs}" := .data[[movenetenv$options$movedata_cols$move_date]])
+#' @param data
+#'
+#' @param prefix
+#'
+#' @export
+anonymise <- function(data, prefix){
+  holding_IDs <- unique(c(data[[movenetenv$options$movedata_cols$origin_ID]], data[[movenetenv$options$movedata_cols$dest_ID]]))
+  holding_IDs <- setNames(paste0(prefix,1:length(holding_IDs)), holding_IDs)
+  data[movenetenv$options$movedata_cols$origin_ID] <- holding_IDs[data[[movenetenv$options$movedata_cols$origin_ID]]]
+  data[movenetenv$options$movedata_cols$dest_ID] <- holding_IDs[data[[movenetenv$options$movedata_cols$dest_ID]]]
+  return(data)
 }
