e3ff45a493620b2e6b9b83285b61aebfb6619f0f
Stefan Widgren <stefan.widgren@sva.se>
2022-11-06 22:39:58 +0100

    Add a 'SEIRcm' class to handle the SEIR contact matrix model

---
+++ R/SEIRcm.R
@@ -0,0 +1,6 @@
+##' Definition of the \sQuote{SEIRcm} model
+##'
+##' Class to handle the SEIRcm model.
+##' @importClassesFrom SimInf SimInf_model
+##' @export
+setClass("SEIRcm", contains = c("SimInf_model"))
