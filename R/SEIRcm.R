7aecef0261fa7cd5978d8c52b4f8bbca7b93b60c
Stefan Widgren <stefan.widgren@sva.se>
2022-11-15 07:01:45 +0100

    Work in progress to create SEIRcm model

--- R/SEIRcm.R
+++ R/SEIRcm.R
@@ -4,3 +4,21 @@
 ##' @importClassesFrom SimInf SimInf_model
 ##' @export
 setClass("SEIRcm", contains = c("SimInf_model"))
+
+##' Create an SEIR model with contact matrix
+##'
+##' @param u0 FIXME
+##' @param tspan FIXME
+##' @param events FIXME
+##' @param beta FIXME
+##' @param epsilon FIXME
+##' @param gamma FIXME
+##' @export
+SEIRcm <- function(u0,
+                   tspan,
+                   events  = NULL,
+                   beta    = NULL,
+                   epsilon = NULL,
+                   gamma   = NULL) {
+    compartments <- c("S", "E", "I", "R")
+}
