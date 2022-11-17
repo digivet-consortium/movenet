3d690546635c3d79e1b2ade049d7e81b613f7500
Stefan Widgren <stefan.widgren@sva.se>
2022-11-17 16:17:54 +0100

    Work in progress with the SEIRcm model

--- R/SEIRcm.R
+++ R/SEIRcm.R
@@ -9,19 +9,51 @@ setClass("SEIRcm", contains = c("SimInf_model"))
 ##'
 ##' @param u0 FIXME
 ##' @param tspan FIXME
-##' @param events FIXME
-##' @param beta FIXME
-##' @param epsilon FIXME
-##' @param gamma FIXME
+##' @param beta the transmission rate from susceptible to infected
+##'     state.
+##' @param epsilon the incubation rate from exposed to infected state.
+##' @param gamma the recovery rate from infected to recovered state.
 ##' @param coupling FIXME
 ##' @export
-SEIRcm <- function(u0,
-                   tspan,
-                   events  = NULL,
+SEIRcm <- function(u0      = NULL,
+                   tspan   = NULL,
                    beta    = NULL,
                    epsilon = NULL,
                    gamma   = NULL) {
     compartments <- c("S", "E", "I", "R")
+
+    ## Check u0
+    if (!is.data.frame(u0))
+        u0 <- as.data.frame(u0)
+    if (!all(compartments %in% names(u0)))
+        stop("Missing columns in u0.", call. = FALSE)
+    u0 <- u0[, compartments, drop = FALSE]
+
+    ## Check beta
+    if (!is.numeric(beta) ||
+        length(beta) != 1 ||
+        !is.finite(beta) ||
+        any(beta < 0)) {
+        stop("'beta' must be a numeric value >= 0.", call. = FALSE)
+    }
+
+    ## Check epsilon
+    if (!is.numeric(epsilon) ||
+        length(epsilon) != 1 ||
+        !is.finite(epsilon) ||
+        any(epsilon < 0)) {
+        stop("'epsilon' must be a numeric value >= 0.", call. = FALSE)
+    }
+
+    ## Check gamma
+    if (!is.numeric(gamma) ||
+        length(gamma) != 1 ||
+        !is.finite(gamma) ||
+        any(gamma < 0)) {
+        stop("'gamma' must be a numeric value >= 0.", call. = FALSE)
+    }
+
+    gdata <- c(beta = beta, epsilon = epsilon, gamma = gamma)
 
     ## Dependency graph.
     G <- matrix(c(
@@ -46,10 +78,11 @@ SEIRcm <- function(u0,
         byrow = TRUE,
         dimnames = list(compartments, NULL))
 
-    model <- SimInf_model(G      = G,
-                          S      = S,
-                          tspan  = tspan,
-                          u0     = u0)
+    model <- SimInf_model(G     = G,
+                          S     = S,
+                          tspan = tspan,
+                          gdata = gdata,
+                          u0    = u0)
 
     as(model, "SEIRcm")
 }
